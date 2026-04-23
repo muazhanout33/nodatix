8a1808d9c4c921e7893c11b0d8181c953a4c5efc
muazhanout33 <muazomohamed2006@gmail.com>
2026-04-23 05:39:16 +0200

    add logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,8 +35,9 @@ export default function Navbar() {
     >
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         {/* Logo */}
-      <a href="#" className="flex items-center gap-2 group">
+      <a href="#" className="flex items-center gap-2 group">  
        <img src="/logo.jpg.jpeg" alt="ScalaryX" style={{ height: "100px", width: "auto", objectFit: "contain" }} />
+        <span>ScalaryX</span>
      </a>
 
         {/* Desktop Nav */}
