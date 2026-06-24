cbc0c0d005fd5371bdbe7704b46862039033d9e0
muazhanout33 <muazomohamed2006@gmail.com>
2026-06-24 03:37:21 +0300

    add logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
            <a href="#" className="flex items-center gap-2 mb-4"> 
   
   <img 
-    src="/Gemini_Generated_Image_vtug5ovtug5ovtug-removebg-preview (1).png" 
+    src="/favicon.ico.png" 
     alt="ScalaryX Logo"
     className="h-10 w-auto object-contain"
     style={{ height: "60px", width: "auto", objectFit: "contain" }}
